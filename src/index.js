9568d22e9da6723e49fc95e4724afea8754a0b6e
Irshad Muhammad <imabbasi@msn.com>
2022-06-23 12:20:15 +0200

    delete from firestore db-collection

--- src/index.js
+++ src/index.js
@@ -1,5 +1,12 @@
 import { initializeApp } from "firebase/app";
-import { getFirestore, collection, getDocs, addDoc } from "firebase/firestore";
+import {
+  getFirestore,
+  collection,
+  getDocs,
+  addDoc,
+  deleteDoc,
+  doc,
+} from "firebase/firestore";
 const firebaseConfig = {
   apiKey: process.env.apiKey,
   authDomain: process.env.authDomain,
@@ -32,11 +39,16 @@ addBookForm.addEventListener("submit", (e) => {
     title: addBookForm.title.value,
     author: addBookForm.author.value,
   }).then(() => {
-    addBookForm.requestFullscreen();
+    addBookForm.reset();
   });
 });
 
 const deleteBookForm = document.querySelector(".delete");
 deleteBookForm.addEventListener("submit", (e) => {
-  e.preventDefault;
+  e.preventDefault();
+
+  const docRef = doc(db, "books", deleteBookForm.id.value);
+  deleteDoc(docRef).then(() => {
+    deleteBookForm.reset();
+  });
 });
